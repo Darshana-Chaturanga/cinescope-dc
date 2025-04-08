7ae8bdaee305ae3be53936ef49d8f3261ae3dd8c
Darshana-Chaturanga <mpdarshanachaturanga@gmail.com>
2025-04-09 04:57:15 +0530

    Day 7 final commit

--- components/header-nav.jsx
+++ components/header-nav.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Logo from "./logo";
+import { ModeToggle } from "./mode-toggle";
 
 export default function HeaderNav() {
   return (
@@ -11,7 +12,7 @@ export default function HeaderNav() {
           <span className="text-primary text-xl font-bold">CineScope</span>         
         </Link>
 
-        <nav className="ml-auto flex  gap-4">
+        <nav className="ml-auto flex items-center  gap-4 ">
           <Link
             href="/"
             className="hover:text-primary text-sm font-medium transition-colors"
@@ -36,6 +37,7 @@ export default function HeaderNav() {
           >
             Admin
           </Link>
+          <ModeToggle/>
         </nav>
       </div>
     </header>
--- components/mode-toggle.jsx
+++ components/mode-toggle.jsx
@@ -1,8 +1,25 @@
+import { Button } from "./ui/button";
+import { Sun } from "lucide-react";
 
 export function ModeToggle() {
+  const theme = "dark";
+
   return (
     <div>
-      ModeToggle
+      <Button
+        variant="ghost"
+        size="icon"
+        // className={cn(
+        //   "h-9 w-9",
+        //   theme === "dark" ? "text-red-500" : "text-blue-500"
+        // )}
+
+        className={`h-9 w-9 ${
+          theme === "dark" ? "text-red-500" : "text-blue-500"
+        }`}
+      >
+        <Sun />
+      </Button>
     </div>
-  )
+  );
 }
